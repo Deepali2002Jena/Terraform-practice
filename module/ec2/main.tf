95e272e15307238d5b5c1fe70b028796b8461a8a
Deepali2002Jena <jenadeepali47@gmail.com>
2025-01-07 16:45:06 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,5 +1,36 @@
-resource "aws_instance" "web" {
-  ami             = "ami-01816d07b1128cd2d"
-  instance_type   = "t3.micro"
- }
+resource "aws_security_group" "new-tf-sg" {
+  name        = "new-sg"
+  description = "allow http and ssh"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-tf-key"
+  public_key = file("/root/tf-prac/deepali.pub")
+}
+
+resource "aws_instance" "my-ec2" {
+  ami           = var.ami-id
+  key_name= aws_key_pair.deployer.id
+  security_groups=["new-sg"]
+  instance_type = "t3.micro"
+}
 
